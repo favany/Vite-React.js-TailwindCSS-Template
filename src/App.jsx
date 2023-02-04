f722166f57d9b152f82e8da16cce1f223feb1e5f
Romance Ucely <faaa@live.com>
2023-02-04 12:16:34 +0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="contents relative h-screen justify-center">
+      <div className="text-blue-600 dark:text-sky-400 font-big text-3xl text-center">
+        Hello, this is React.js + Tailwind CSS, build by Vite!
+      </div>
+    </div>
+  );
+}
+
